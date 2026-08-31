d00e72b8bc00917823f072af1a6162baa5ce18d3
Sai Gunaranjan Pelluri <36566333+SaiGunaranjan@users.noreply.github.com>
2026-09-01 01:32:21 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,36 @@
+# Coded on 01/09/2026 using brute force method
+# Loop through each linked list and keep appending the values to a list. 
+# Sort the list and then create a new linked list from the sorted list of elements
+# This will be O(n*k) extra space and time complexity for sorting the full list again!
+# This works but is not optimal. We can do better using hint 3. 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        temp_list = []
+        for list_ in lists:
+            curr = list_
+            while curr is not None:
+                temp_list.append(curr.val)
+                curr = curr.next
+        
+
+        temp_list.sort()
+
+        dummy = ListNode()
+        prev = dummy
+        for ele in temp_list:
+            prev.next = ListNode(val=ele)
+            prev = prev.next
+        
+        prev.next = None
+
+        return dummy.next
+
+        
