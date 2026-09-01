ce4552d4db58e7e09d0e1fbca88d07b143399b83
Sai Gunaranjan Pelluri <36566333+SaiGunaranjan@users.noreply.github.com>
2026-09-02 02:04:00 +0530

    Add: merge-k-sorted-linked-lists - submission-7

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-7.py
@@ -0,0 +1,52 @@
+# Coded on 02/09/2026
+# This does the divide and conquer approach and the time complexity is O(Nlogk), where N is the total number of nodes and k is the number of elements in the list
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            merged_list = []
+            for i in range(0,len(lists),2):
+                list1 = lists[i]
+                list2 = lists[i+1] if i+1 < len(lists) else None
+                merged_list.append(self.merge2LinkedLists(list1,list2))
+            lists = merged_list
+
+        return lists[0]
+
+    def merge2LinkedLists(self, list1, list2):
+
+        dummy = ListNode()
+        prev = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        
+        if list1 is None:
+            prev.next = list2
+        else:
+            prev.next = list1
+        
+        return dummy.next
+
+
+
+
+        
+
+
+        
